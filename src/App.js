34cc63d2e9d30a93a9d841aebe87ec838440f975
ManishMarvel <manishsesetty@gmail.com>
2021-11-08 14:54:13 +0530

    making greek concise

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
+import Greeting from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <Greet/>
+      <Greeting />
+
 
     </div>
   );
