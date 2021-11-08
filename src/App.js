2a318f6e370928f86391066390427ed737c7f1e2
ManishMarvel <manishsesetty@gmail.com>
2021-11-08 15:05:17 +0530

    passing properties to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Greeting from './components/Greet';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+
 
 function App() {
   return (
     <div className="App">
-      <Greeting />
+      <Greet name="Harish" college="IIT">
+        <p> this is children property</p>
+      </Greet>
+      <Greet name="Kumar" college="Oxford" />
+      <Greet name="Vijay" college="GITAM" />
 
+      <Welcome />
 
     </div>
   );
