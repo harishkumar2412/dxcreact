d54cc132a17c1e8ba45c9175ed169fb3b826ed97
ManishMarvel <manishsesetty@gmail.com>
2021-11-08 15:34:07 +0530

    handling state change

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
-
+import Message from './components/Message';
+ 
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Harish" college="IIT">
-        <p> this is children property</p>
-      </Greet>
-      <Greet name="Kumar" college="Oxford" />
-      <Greet name="Vijay" college="GITAM" />
+    <Message/>
 
-      <Welcome />
+    <Greet name = "Harish"  college = "IIT">
+      <p> this is children property</p>
+    </Greet>
+
+    <Greet name = "Kumar" college = "Oxford">
+      <button>click me</button>
+    </Greet>
+    <Greet />
+
+    <Welcome name ="Vijay" alias = "ansari"/>
 
     </div>
   );
