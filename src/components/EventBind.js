661417ec0665382469de18a9e3a09cafd11fd410
ManishMarvel <manishsesetty@gmail.com>
2021-11-09 12:20:38 +0530

    binding in constructor

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -7,22 +7,25 @@ class EventBind extends Component {
         this.state = {
             message: 'Hello'
         }
+        this.clickHandler = this.clickHandler.bind(this)
     }
 
-clickHandler(){
-    console.log(this)
-    this.setState({
-        message: 'good bye'
-    })
-    
-}
+    clickHandler() {
+        console.log(this)
+        this.setState({
+            message: 'good bye'
+        })
+
+    }
 
     render() {
-        //using bind im tying clickhandler to this keyword
+        //using bind im tying clickhandler to this keyword -- binding in render method
         return (
             <div>
                 <div>{this.state.message}</div>
-                <button onClick = {this.clickHandler.bind(this)}>click</button>  
+                { /*<button onClick = {this.clickHandler.bind(this)}>click</button>  */}
+                {/* <button onClick = {() => this.clickHandler()}>click</button>   */}
+                <button onClick={this.clickHandler}>click</button>
             </div>
         )
     }
