01a16c0f0d9b94ad2741aa275c9e8747509283fb
ManishMarvel <manishsesetty@gmail.com>
2021-11-10 11:32:54 +0530

    styling css

--- src/App.js
+++ src/App.js
@@ -10,11 +10,15 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Table from './components/Table';
+import Stylesheet from './components/Stylesheet';
  
 
 function App() {
   return (
     <div className="App">
+    <Stylesheet/>
+    <Table/>
     <NameList/>
     <UserGreeting/>
 
