9a7b35a098f939f82aa89ceb0e0d83dfdd18746f
ManishMarvel <manishsesetty@gmail.com>
2021-11-12 15:49:40 +0530

    forwarding refs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import FocusInput from './components/refs/FocusInput';
-import Input from './components/refs/Input';
+import FrParentInput from './components/refs/FrParentInput';
 
-import RefsDemo from './components/RefsDemo';
 
 
 function App() {
   return (
     <div className="App">
-    <RefsDemo/>
-      <FocusInput/>
+    <FrParentInput/>
     </div>
   );
 }
+
 export default App;
