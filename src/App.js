ce7db7cf1a963025c7b718dd3210946d394f43a1
ManishMarvel <manishsesetty@gmail.com>
2021-11-12 14:43:24 +0530

    refs demo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Table from './components/Table';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyles.css'
-import styles from './appStyles.module.css'
-import Form from './components/Form';
-import Fragments from './components/Fragments';
-import NewTable from './components/NewTable';
-import PureComp from './components/PureComp';
-import Parentcomp from './components/Parentcomp';
+
+import RefsDemo from './components/RefsDemo';
 
 
 function App() {
   return (
     <div className="App">
-    <Parentcomp/>
+    <RefsDemo/>
     </div>
   );
 }
--- src/components/Parentcomp.js
+++ src/components/Parentcomp.js
@@ -12,10 +12,16 @@ constructor(props) {
     }
 }
 
+/**
+ * im setting the state after every 2 secs,
+ * but im setting the same value in the name field of the state
+ * regular component re renders though the value in the state's name field hasn't changed
+ * but a pure component will not re-render --- because it'll do a shallow comparison
+ */
     componentDidMount(){
         setInterval(() => {
             this.setState({
-                name: 'abdul-parentcomp-memo'
+                name: 'abdul-parentcomnew'
             })
         }, 2000);
     }
