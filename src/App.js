55f932184d40abef9802a28cbf46b95acb0e5d75
ManishMarvel <manishsesetty@gmail.com>
2021-11-17 13:12:18 +0530

    useState with object and spread operator

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import HookCounter2 from './components/hooks/HookCounter2';
+import HookCounter3 from './components/hooks/HookCounter3';
 
 
 
@@ -8,7 +9,7 @@ import HookCounter2 from './components/hooks/HookCounter2';
 function App() {
   return (
     <div className="App">
-    <HookCounter2/>
+    <HookCounter3/>
     </div>
   );
 }
