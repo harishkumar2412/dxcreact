ac5ff6ff9d0bb32ae1ec4cd8de576bb47a0694c0
ManishMarvel <manishsesetty@gmail.com>
2021-11-17 15:38:16 +0530

    updating conditionally

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import CounterHook1 from './components/hooks/useeffect/CounterHook1';
 function App() {
   return (
     <div className="App">
-    <CounterHook1/>
+    <ClassCounter1/>
     </div>
   );
 }
