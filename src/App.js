9736da1d4f7f880d43835e1eff4c8af1ac0888fa
ManishMarvel <manishsesetty@gmail.com>
2021-11-17 16:20:00 +0530

    run effects only once

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import ClassCounter1 from './components/hooks/useeffect/ClassCounter1';
+import ClassMouse from './components/hooks/useeffect/ClassMouse';
 import CounterHook1 from './components/hooks/useeffect/CounterHook1';
+import HookMouse from './components/hooks/useeffect/HookMouse';
 
 
 
@@ -9,7 +11,7 @@ import CounterHook1 from './components/hooks/useeffect/CounterHook1';
 function App() {
   return (
     <div className="App">
-      <CounterHook1 />
+      <HookMouse/>
     </div>
   );
 }
