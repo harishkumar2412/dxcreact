195fa7b9a8f79c9d7bab6d7f5ca2703829f292b6
ManishMarvel <manishsesetty@gmail.com>
2021-11-18 11:05:33 +0530

    getting and showing posts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
-import ClassCounter1 from './components/hooks/useeffect/ClassCounter1';
-import ClassMouse from './components/hooks/useeffect/ClassMouse';
-import CounterHook1 from './components/hooks/useeffect/CounterHook1';
-import HookMouse from './components/hooks/useeffect/HookMouse';
-import MouseContainer from './components/hooks/useeffect/MouseContainer';
+import DataFetching from './components/hooks/useeffect/DataFetching';
+
 
 
 
@@ -12,7 +9,7 @@ import MouseContainer from './components/hooks/useeffect/MouseContainer';
 function App() {
   return (
     <div className="App">
-      <MouseContainer />
+        <DataFetching/>
     </div>
   );
 }
