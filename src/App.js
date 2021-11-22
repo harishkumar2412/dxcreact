d022bddbeec1f96c182a7998dcec8f2324553a76
ManishMarvel <manishsesetty@gmail.com>
2021-11-22 12:00:19 +0530

    two reducers to manage 2 different counters

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import CounterTwo from './components/reducerhook/CounterTwo';
+import CounterThree from './components/reducerhook/CounterThree';
 
 
 
 function App() {
   return (
     <div className="App">
-    <CounterTwo/>
+      <CounterThree />
     </div>
   );
 }
