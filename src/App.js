3ea7553ddf9c4146f51e7c54a071c90b4a7577de
ManishMarvel <manishsesetty@gmail.com>
2021-11-22 11:11:33 +0530

    passing additional value to action field

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import CounterOne from './components/reducerhook/CounterOne';
+import CounterTwo from './components/reducerhook/CounterTwo';
 
 
 
 function App() {
   return (
     <div className="App">
-     <CounterOne/>
+    <CounterTwo/>
     </div>
   );
 }
