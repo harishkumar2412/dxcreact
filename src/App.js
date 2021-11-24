50a23927e2f8bf523885a861d80e1c1e63c86fa6
ManishMarvel <manishsesetty@gmail.com>
2021-11-24 10:17:46 +0530

    life b4 higher order functions-1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useReducer } from 'react';
-import CounterTwo from './components/reducerhook/CounterTwo';
-import CounterThree from './components/reducerhook/CounterThree';
-import CompA from './components/contextNreducer/CompA';
-import CompB from './components/contextNreducer/CompB';
-import CompC from './components/contextNreducer/CompC';
-import DataFectching from './components/useReducercases/DataFectching';
-import DataFetchingTwo from './components/useReducercases/DataFetchingTwo';
+import React from 'react';
+import ClickCounter from './components/higherorderComponents/ClickCounter';
 
-export const CountContext = React.createContext()
-const initialState = 0
 
-const reducer = (state, action) => {
-  switch (action) {
-    case 'increment': return state + 1
-    case 'decrement': return state - 1
-    case 'reset': return initialState
-    default: return state
-  }
-}
+
+
 
 function App() {
   return (
 
     <div className="App">
-      <DataFetchingTwo />
+     <ClickCounter/>
     </div>
 
   );
